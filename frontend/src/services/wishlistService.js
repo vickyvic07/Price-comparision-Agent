8970b296d148d34f24d390100ea84b937e84ac1c
Vignesh <vicktvicky101@gmail.com>
2026-07-06 23:12:44 +0530

    Delete frontend directory

--- frontend/src/services/wishlistService.js
+++
@@ -1,7 +0,0 @@
-import api from './api';
-
-export const addToWishlist    = (data)   => api.post('/wishlist', data).then((r) => r.data);
-export const getWishlist      = (userId) => api.get(`/wishlist/${userId}`).then((r) => r.data);
-export const removeWishlist   = (itemId) => api.delete(`/wishlist/${itemId}`).then((r) => r.data);
-export const updateThreshold  = (itemId, data) =>
-  api.patch(`/wishlist/${itemId}`, data).then((r) => r.data);
